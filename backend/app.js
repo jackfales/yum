fcd42824819831a76cc288a0a64a931eee49721c
Sam <samgfales@gmail.com>
2023-09-14 14:08:37 -0700

    added virtualenv

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Sam!');
-}).listen(8080);
-
-// Testing Testing Testing
