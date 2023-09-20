dfcbbed8e5bff105646b049ac158cc32880fe71c
Derek Tran <tran.derek.d@gmail.com>
2023-09-20 10:26:41 -0700

    Create components for form inputs

--- pages/create-account.js
+++ pages/create-account.js
@@ -9,22 +9,24 @@ export default function Account() {
       <main>
         <div>
           <form method='POST' action=''>
-            <div>
-              <label htmlFor='username'>Username:</label>
-              <input type='text' id='username' name='username'></input>
-            </div>
-            <div>
-              <label htmlFor='password'>Password:</label>
-              <input type='text' id='password' name='password'></input>
-            </div>
-            <div>
-              <label htmlFor='confirmpassword'>Confirm Password:</label>
-              <input type='text' id='confirmpassword' name='confirmpassword'></input>
-            </div>
+            <Input name='Username' type='text'></Input>
+            <Input name='Email' type='text'></Input>
+            <Input name='Password' type='text'></Input>
+            <Input name='Confirm Password' type='text'></Input>
             <button type='submit'>Create Account</button>
           </form>
         </div>
       </main>
     </div>
   )
+
+  function Input({name, type}) {
+    let nameSpacesRemoved = name.replace(/\s/g, '').toLocaleLowerCase();
+    return (
+      <div>
+        <label htmlFor={nameSpacesRemoved}>{name}:</label>
+        <input type={type} id={nameSpacesRemoved} name={nameSpacesRemoved}></input>
+      </div>
+    )
+  }
 }
