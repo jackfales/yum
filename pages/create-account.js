cc9c268a02c05ee45c070753b95cdd750cbd1e0d
Derek Tran <tran.derek.d@gmail.com>
2023-09-21 10:14:03 -0700

    Add styles to help readability

--- pages/create-account.js
+++ pages/create-account.js
@@ -40,7 +40,7 @@ export default function Account() {
             break;
         case 'pword':
           errorMessage = inputs[input].length < 5 ?
-            'Password must be at least 6 characters longer!' : '';
+            'Password must be at least 6 characters long!' : '';
           break;
         case 'confirmpword':
           errorMessage = inputs['pword'] !== inputs[input] ?
@@ -63,41 +63,85 @@ export default function Account() {
       <Head>
           <title>Create Account</title>
       </Head>
-      <main>
+      <main className={styles.container}>
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor='fname'>First name:</label>
             <input type='text' name='fname' onChange={handleChange}/>
-            <div style={{color: 'red', height: '25px'}}>{errors.fname}</div>
+            <div className='error'>{errors.fname}</div>
           </div>
           <div>
             <label htmlFor='lname'>Last name:</label>
             <input type='text' name='lname' onChange={handleChange}/>
-            <div style={{color: 'red', height: '25px'}}>{errors.lname}</div>
+            <div className='error'>{errors.lname}</div>
           </div>
           <div>
             <label htmlFor='uname'>Username:</label>
             <input type='text' name='uname' onChange={handleChange}/>
-            <div style={{color: 'red', height: '25px'}}>{errors.uname}</div>
+            <div className='error'>{errors.uname}</div>
           </div>
           <div>
             <label htmlFor='pword'>Password:</label>
             <input type='password' name='pword' onChange={handleChange}/>
-            <div style={{color: 'red', height: '25px'}}>{errors.pword}</div>
+            <div className='error'>{errors.pword}</div>
           </div>
           <div>
             <label htmlFor='confirmpword'>Confirm Password:</label>
             <input type='password' name='confirmpword' onChange={handleChange}/>
-            <div style={{color: 'red', height: '25px'}}>{errors.confirmpword}</div>
+            <div className='error'>{errors.confirmpword}</div>
           </div>
           <button type='submit'>Create Account</button>
         </form>
       </main>
       <style jsx>{`
-      label {
-        display: block;
-      }
+        main {
+        }
+
+        label {
+          display: block;
+          font-size: 18px;
+        }
+
+        form {
+          padding: 30px;
+          width: 300px;
+        }
+
+        input, button {
+          width: 100%;
+          height: 25px;
+        }
+
+        .error {
+          text-wrap: wrap;
+          height: 45px;
+          color: #FA535E;
+        }
       `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+      
     </div>
   )
 }
