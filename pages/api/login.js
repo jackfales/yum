ec4781d5d73e3477cea8c5a5fcaabc52f27b7f15
Sam <samgfales@gmail.com>
2023-10-09 20:01:22 -0600

    changed unused variable

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,7 +7,7 @@ export default async function login(req, res) {
   const { username, password } = JSON.parse(req.body);
 
   try {
-    const user = await Auth.signIn(username, password);
+    await Auth.signIn(username, password);
     return res.status(200).json({ success: true });
   } catch (error) {
     return res.status(400).json({ success: false, message: error.toString() });
