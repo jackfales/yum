63d959436bbe77ad1414cc8af3d0fcc005de5a18
Derek Tran <tran.derek.d@gmail.com>
2023-10-12 10:16:39 -0700

    Retrieve data associated with dynamic route

--- app/[user]/page.tsx
+++ app/[user]/page.tsx
@@ -1,13 +1,40 @@
+// These imports just for grabbing the static data - these should be removed
+import path from 'path';
+import fs from 'fs';
+
 export const metadata = {
   title: 'Dashboard'
 }
 
+const profileDirectory: string = path.join(process.cwd(), 'data');
+
 // Grab all users, this would probably query the user database
 export async function generateStaticParams() {
-  return [{ user: 'dtran'}, { user: 'jfales'}, { user: 'sfales'}];
+  const fileNames: string[] = fs.readdirSync(profileDirectory);
+  return fileNames.map((fileName) => {
+    return { 
+      params: { user: fileName.replace(/\.json$/, '') }
+    }
+  })
+}
+
+// Grabs profile data associated with user, probably another query
+export function getProfileData(id: string) {
+  const profilePath: string = path.join(profileDirectory,`${id}.json`);
+  return JSON.parse(fs.readFileSync(profilePath, 'utf8'));
 }
 
 export default async function Profile({ params }: { params: {user: string}}) {
   const { user } = params;
-  return <h1>Hi this is {user}.</h1>
+  const { followers, following, postCount } = getProfileData(user);
+  return (
+    <>
+      <h1>Hi this is {user}</h1>
+      <ul>
+        <li>Followers: {followers}</li>
+        <li>Following: {following}</li>
+        <li>Post Count: {postCount}</li>
+      </ul>
+    </>
+  )
 }
