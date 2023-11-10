5bb86fc3c75cebdfac9f31efaa322599d6cf3e2f
Derek Tran <tran.derek.d@gmail.com>
2023-11-10 11:08:00 -0800

    Restrict Post creation to auth'd users

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,24 +1,42 @@
-import { NextResponse } from "next/server"
-
-type Data = {
-  url?: string,
-  name?: string,
-  caption?: string,
-  recipe?: string,
-  ingredients?: string,
-  tags?: string
-}
+import { headers } from 'next/headers';
+import { NextResponse } from "next/server";
+import { withSSRContext } from "aws-amplify";
 
 export async function POST(request: Request) {
-  const data: Object = (Object.fromEntries(await request.formData()));
+  // Checks if the request comes from an authenticated user
+  const req = {
+    headers: {
+      cookie: headers().get("cookie")
+    },
+  };
+
+  const { Auth } = withSSRContext({ req });
+
+  try {
+    await Auth.currentAuthenticatedUser();
+  } catch(err) {
+    console.log(err);
+    return NextResponse.json(
+      {error: 'You do not have permission to access this resource'}, 
+      {status: 403}
+    );
+  }
+
+  const payload: Object = (Object.fromEntries(await request.formData()));
 
   const res = await fetch('https://wb07xao9oa.execute-api.us-west-2.amazonaws.com/dev/post', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(data)
+    body: JSON.stringify(payload)
   });
+  
+  const data = await res.json();
 
-  console.log(await res.json());
-
-  return NextResponse.json(res);
+  const body = JSON.parse(data['body']);
+  if (data.statusCode == 201) {
+    return NextResponse.json({url: `${body['url']}`}, {status: data.statusCode});
+  } else {
+    const errorMessage = `${body['errorType']}: ${body['errorMessage'][1]}`;
+    return NextResponse.json({error: errorMessage}, {status: data.statusCode})
+  }
 }
