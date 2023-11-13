e43035ba116f20a8fc60bb83eeba5c034a5ea0a0
sammyoto <samgfales@gmail.com>
2023-11-12 20:23:16 -0700

    finalized edits on Edit profile page frontend

--- app/dashboard/edit-profile/page.tsx
+++ app/dashboard/edit-profile/page.tsx
@@ -1,8 +1,8 @@
-import { withSSRContext } from "aws-amplify";
+ import { withSSRContext } from "aws-amplify";
 import { headers } from "next/headers";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 import styles from "../../../styles/Dashboard.module.css";
+import EditProfileForm from "../../components/EditProfileForm";
 
 export const metadata = {
   title: 'EditProfile'
@@ -29,54 +29,13 @@ export default async function EditProfile() {
     redirect('/login');
   }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target)
-
-  }
-
-  /* NEEDS
-    - First Name
-    - Last Name
-    - Username
-    - Gender
-    - Bio
-    - Profile Pic
-  */
-  
-  return (
+  return (<>
     <div>
         <div id={styles.header} className={`${styles.container} ${styles.spacebetween}`}>
         <div>EDIT PROFILE</div>
+        </div>
     </div>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <h3>Profile Picture</h3>
-                <input type='image' name='profilePicture'/>   
-            </div>
-            <div>
-                <h3>First Name</h3>
-                <input type='text' name='firstName'/>   
-            </div>
-            <div>
-                <h3>Last Name</h3>
-                <input type='text' name='lastName'/>   
-            </div>
-            <div>
-                <h3>Username</h3>
-                <input type='text' name='username'/>   
-            </div>
-            <div>
-                <h3>Gender</h3>
-                <input type='text' name='gender'/>   
-            </div>
-            <div>
-                <h3>Bio</h3>
-                <input type='text' name='bio'/>   
-            </div>
-            <button type='submit'>Save Changes</button>
-        </form>
-    </div>
+    <EditProfileForm></EditProfileForm>
+  </>
   )
 }
