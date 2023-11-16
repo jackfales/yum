62aeeb465ca8a6cbb2e2da74ce34c7177bfc8f88
Derek Tran <tran.derek.d@gmail.com>
2023-11-16 08:54:24 -0800

    Add Google fonts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import styles from "../../styles/Navbar.module.css";
+import { Playfair } from "next/font/google"
+
+const playfair = Playfair({
+  subsets: ['latin'],
+  weight: ['700']
+})
 
 export default function Navbar({username}: {username: String}) {
   return (<>
     <div id={styles.navbar}>
       <div>ICON</div>
-      <div>YUM</div>
+      <h1 className={playfair.className}>Yum</h1>
       <a className={styles.profilepicture} href={`/${username}`}>
         <Image className={styles.profilepicture}
           src={`/images/pp/${username}.jpg`}
