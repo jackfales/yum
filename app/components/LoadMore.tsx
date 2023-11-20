9e6371a7a3f2c7f62db34cb24c93719cbb177bea
Derek Tran <tran.derek.d@gmail.com>
2023-11-20 09:18:51 -0800

    Moved function to /utils and clean up

--- app/components/LoadMore.tsx
+++ app/components/LoadMore.tsx
@@ -4,25 +4,18 @@ import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import Spinner from './Spinner';
 import Post from "./Post";
+import fetchPosts from "../utils/fetchPosts"
 
-/*TODO: Instead of passing in postData as a Prop to this component, this comp.
+/*TODO: Instead of passing in postData as a Prop to this component, this component
  * should call an API route that returns the posts.
  */
 export default function LoadMore({postsData}: {postsData: Object[]}) {
-  // This state keeps track of post data
   const [posts, setPosts] = useState<Object[]>([]);
-  // This state keeps track of what page we are on
   const [pagesLoaded, setPagesLoaded] = useState(0);
-
-  function fetchPosts(page: number): Object[] {
-    const perPage = 5;
-    const posts = postsData.slice(perPage * page, perPage * page + perPage);
-    return posts;
-  }
 
   const loadMore = () => {
     const nextPage = pagesLoaded + 1;
-    const nextPosts = fetchPosts(nextPage) ?? [];
+    const nextPosts = fetchPosts(nextPage, postsData) ?? [];
     setPosts((prevPosts: Object[]) => [...prevPosts, ...nextPosts]);
     setPagesLoaded(nextPage);
   }
