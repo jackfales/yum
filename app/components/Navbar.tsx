86f004e51eb199f1e3a2ac2a258a864a5601b3c2
Derek Tran <tran.derek.d@gmail.com>
2023-11-26 11:31:30 -0800

    Add missing Navbar shadow

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const playfair = Playfair({
 
 export default function Navbar({username}: {username: String}) {
   return (<>
-    <div className='flex justify-between items-center fixed top-0 left-0 w-full h-12 px-5 bg-rose-200 text-emerald-600'>
+    <div className='flex justify-between items-center fixed top-0 left-0 w-full h-12 px-5 shadow bg-rose-200 text-emerald-600'>
       <div>ICON</div>
       <h1 className={`${playfair.className} text-4xl`}>Yum</h1>
       <ProfileIcon username={username}/>
