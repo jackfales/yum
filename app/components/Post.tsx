80c9ecffa9027b5ce1f9869026177247e31edde1
Derek Tran <tran.derek.d@gmail.com>
2023-11-26 11:27:10 -0800

    Add Tailwind styling to Post

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,5 +1,4 @@
-import styles from "../../styles/Post.module.css";
-import Image from "next/image";
+import ProfileIcon from "./ProfileIcon";
 import { Varela_Round } from "next/font/google";
 
 const varela_round = Varela_Round({
@@ -9,31 +8,25 @@ const varela_round = Varela_Round({
 
 export default function Post({name}: {name: String}) {
   return (<>
-    <div id={styles.post} className={varela_round.className}>
-      <div id={styles.header}>
-        <div id={styles.title}>{name}</div>
+    <div className={`${varela_round.className} flex flex-col border border-solid border-gray-200 rounded-lg my-5 transition ease-in-out duration-500 hover:shadow-xl`}>
+      <div className='flex justify-between items-center h-14 px-5'>
+        <div className='flex-auto text-2xl'>{name}</div>
       </div>
-      <div id={styles.content}>
-        <img
+      <div className='shrink basis-[680px] w-full min-h-[500px] overflow-hidden'>
+        <img className='w-full h-full object-cover'
           src="https://picsum.photos/680"
           alt="Example"
         />
       </div>
-      <div id={styles.footer}>
-        <a id={styles.user} href={`/dtran`}>
-          <Image
-            id={styles.profilepicture}
-            src={`/images/pp/dtran.jpg`}
-            alt="Picture of the user"
-            width={40}
-            height={40}
-          />
-          <span id={styles.username}>dtran</span>
-        </a>
-        <div id={styles.buttons}>
-          <div id={styles.like}>L</div>
-          <div id={styles.comment}>C</div>
-          <div id={styles.share}>S</div>
+      <div className='flex justify-between items-center h-14 px-5'>
+        <div className='flex gap-2.5 items-center text-lg'>
+          <ProfileIcon username='dtran'/>
+          <span>dtran</span>
+        </div>
+        <div className='flex gap-2.5 items-center'>
+          <div className='flex justify-center items-center border border-solid border-gray-200 w-10 h-10 rounded-full text-gray-200 transition ease-in-out duration-500 hover:border-gray-400 hover:text-gray-400'>L</div>
+          <div className='flex justify-center items-center border border-solid border-gray-200 w-10 h-10 rounded-full text-gray-200 transition ease-in-out duration-500 hover:border-gray-400 hover:text-gray-400'>C</div>
+          <div className='flex justify-center items-center border border-solid border-gray-200 w-10 h-10 rounded-full text-gray-200 transition ease-in-out duration-500 hover:border-gray-400 hover:text-gray-400'>S</div>
         </div>
       </div>
     </div>
