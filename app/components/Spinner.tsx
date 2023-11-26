d1089126b846edd86096dabb80eb1993db773ff7
Derek Tran <tran.derek.d@gmail.com>
2023-11-26 11:45:51 -0800

    Add Tailwind styling to Spinner

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,8 +1,6 @@
-import styles from "../../styles/Spinner.module.css";
-
 export default function Spinner() {
   return (<>
-      <div id={styles.spinner}></div>
+      <div className='w-12 h-12 rounded-full border-[7px] border-gray-100 border-t-[7px] border-t-rose-200 animate-[spin_1s_ease_infinite]'></div>
     </>
   )
 }
