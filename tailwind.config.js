161d9b338973da4f1234ab0eab0bb1fdb85fb5ed
Derek Tran <tran.derek.d@gmail.com>
2023-11-28 11:42:53 -0800

    Add custom color and Tailwind styling to Home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: {
+          100: '#f9f9f7',
+          200: '#f4f3ef',
+          300: '#efeee8',
+          400: '#eae8e0',
+          500: '#e5e3d9',
+        },
+      },
+    },
   },
   plugins: [],
 }
