6ff069971193417b7b54e8f01cdd185d09a105d2
Sam <samgfales@gmail.com>
2023-12-20 15:55:58 -0800

    fixed post function.

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,18 +1,21 @@
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import { withSSRContext } from "aws-amplify";
+
+const neptune_url = "https://dzcmprdreb.execute-api.us-west-2.amazonaws.com/api/users"
 
 export async function POST(request: Request) {
-  // Checks if the request comes from an authenticated user
 
-  // TODO: parse form data and send appropriate API request
-  const payload: Object = (Object.fromEntries(await request.formData()));
+  var response = "Request Failed"
 
-  const res = await fetch('https://dzcmprdreb.execute-api.us-west-2.amazonaws.com/api/users', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(payload)
-  });
+  await request.json()
+  .then( async data => {
 
-  const data = await res.json();
+    const res = await fetch(`${neptune_url}`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    });
+
+    response = await res.json();
+  })
+  return NextResponse.json({response})
 }
