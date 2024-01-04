c304e4a0efaa8d353b03a4943b76f2c63148e0ce
Sam <samgfales@gmail.com>
2024-01-04 15:18:01 -0800

    fixed putUser error messages and editProfileForm auto population

--- app/components/EditProfileForm.tsx
+++ app/components/EditProfileForm.tsx
@@ -57,7 +57,6 @@ export default function EditProfileForm({userData}) {
     const status = res.status
 
     if (status == 200) {
-      setErrorMessage("")
       router.push("/dashboard")
     }
     else {
@@ -72,15 +71,15 @@ export default function EditProfileForm({userData}) {
               <label htmlFor='profilePicture' className='font-semibold'>Profile Picture:</label>
               <input type='file' name='profilePicture' className='w-full mb-2'/>
               <label htmlFor='firstName' className='font-semibold'>First Name:</label>
-              <input type='text' placeholder = {firstName} name='firstName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue= {firstName} name='firstName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='lastName' className='font-semibold'>Last Name:</label>
-              <input type='text' placeholder = {lastName} name='lastName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue= {lastName} name='lastName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='username' className='font-semibold'>Username:</label>
-              <input type='text' placeholder = {username} name='username' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue= {username} name='username' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='gender' className='font-semibold'>Gender:</label>
-              <input type='text' placeholder = {gender} name='gender' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue= {gender} name='gender' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='bio' className='font-semibold'>Bio:</label>
-              <input type='text' placeholder = {bio} name='bio' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue= {bio} name='bio' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
             <button type='submit' className='my-2 py-2 w-full border bg-emerald-500 transition-colors hover:bg-emerald-600 rounded-full text-white text-lg font-semibold text-center'>Save Changes</button>
         </form>
     </>
