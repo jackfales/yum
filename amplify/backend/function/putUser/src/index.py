c4f3190dabefee0caca751df23906191e800e36d
Sam <samgfales@gmail.com>
2024-01-07 14:44:47 -0800

    fixed badAttribute bug for putUser. also fixed status code errors.

--- amplify/backend/function/putUser/src/index.py
+++ amplify/backend/function/putUser/src/index.py
@@ -9,6 +9,15 @@ port = os.environ['NEPTUNE_PORT']
 # Connect to Neptune
 db = client.Client(f"wss://{endpoint}:{port}/gremlin", "g")
 
+valid_attributes = [
+    "firstName",
+    "lastName",
+    "username",
+    "DOB",
+    "gender",
+    "bio"
+]
+
 def lambda_handler(event, context):
     userData = json.loads(event["body"])["attributes"]
     username = event["pathParameters"]["userId"]
@@ -16,25 +25,27 @@ def lambda_handler(event, context):
     # Get vertex ID
     query = f"g.V().has('username', '{username}').id();"
     vertexID = db.submit(query).all().result()
-    result = ""
 
-    if len(userData) > 0:
+    result = ""
+    statusCode = 500
+    if len(vertexID) == 0:
+        result = f"Server failed to find user : {username}"
+        statusCode = 404
+    elif len(userData) > 0:
         for attribute in userData:
             newValue = userData[attribute]
-            # Add new value
             query = f"g.V({vertexID}).property(single, '{attribute}', '{newValue}');"
-            try:
-                if len(vertexID) == 0:
-                    result = f"Server failed to find user : {username}"
-                    statusCode = 404
-                    break
-                else:
+            # check if attribute is valid
+            if attribute in valid_attributes:
+                try:
                     db.submit(query)
                     result += f"User attribute : \"{attribute}\" successfully changed to : \"{newValue}\"\n"
+                    # if one query works, status code is 200, else status code is 500
                     statusCode = 200
-            except:
-                result += f"Server failed at modifying attribute : \"{attribute}\" for user : \"{username}\"\n"
-                statusCode = 500
+                except:
+                    result += f"Server failed at modifying attribute : \"{attribute}\" for user : \"{username}\"\n"
+            else:
+                result += f"Attribute : \"{attribute}\" is not a valid attribute\n"
     else:
         result = f"No changes were made to user : \"{username}\""
         statusCode = 200
--- app/components/EditProfileForm.tsx
+++ app/components/EditProfileForm.tsx
@@ -71,15 +71,15 @@ export default function EditProfileForm({userData}) {
               <label htmlFor='profilePicture' className='font-semibold'>Profile Picture:</label>
               <input type='file' name='profilePicture' className='w-full mb-2'/>
               <label htmlFor='firstName' className='font-semibold'>First Name:</label>
-              <input type='text' defaultValue= {firstName} name='firstName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue={firstName} name='firstName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='lastName' className='font-semibold'>Last Name:</label>
-              <input type='text' defaultValue= {lastName} name='lastName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue={lastName} name='lastName' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='username' className='font-semibold'>Username:</label>
-              <input type='text' defaultValue= {username} name='username' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue={username} name='username' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='gender' className='font-semibold'>Gender:</label>
-              <input type='text' defaultValue= {gender} name='gender' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue={gender} name='gender' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
               <label htmlFor='bio' className='font-semibold'>Bio:</label>
-              <input type='text' defaultValue= {bio} name='bio' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
+              <input type='text' defaultValue={bio} name='bio' className='w-full border border-gray-200 shadow-inner mb-2 py-1 px-2 rounded-md'/>   
             <button type='submit' className='my-2 py-2 w-full border bg-emerald-500 transition-colors hover:bg-emerald-600 rounded-full text-white text-lg font-semibold text-center'>Save Changes</button>
         </form>
     </>
