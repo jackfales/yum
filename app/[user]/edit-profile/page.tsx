e144d160725acedeb379030fe2570e01ad57f340
Sam <samgfales@gmail.com>
2024-01-12 15:38:28 -0800

    Fixed small error, using userID instead of username in navbar.

--- app/[user]/edit-profile/page.tsx
+++ app/[user]/edit-profile/page.tsx
@@ -20,9 +20,11 @@ export default async function EditProfile() {
 
   // Renders edit profile page if authenticated, else redirect to login
   let userID;
+  let username;
   try {
     const data = await Auth.currentAuthenticatedUser();
      userID  = data.attributes.sub;
+     username = data.username;
   } catch(err) {
     console.log(err);
     redirect('/login');
@@ -35,7 +37,7 @@ export default async function EditProfile() {
   const data = await res.json();
 
   return (<>
-    <Navbar username={userID}/>
+    <Navbar username={username}/>
     <main className='bg-cream-100 h-screen flex justify-center items-center pt-12'>
       <div className='flex flex-col justify-center items-start w-80'>
         <h1 className='text-5xl tracking-tight font-bold mb-4 text-neutral-800'>Edit Profile</h1>
