730713cf8aa75b98e9f6231490ff57031ef1c721
Derek Tran <tran.derek.d@gmail.com>
2024-01-11 20:54:26 -0800

    Pull changes and add storage

--- amplify/backend/function/postUser/src/index.py
+++ amplify/backend/function/postUser/src/index.py
@@ -10,27 +10,27 @@ port = os.environ['NEPTUNE_PORT']
 db = client.Client(f"wss://{endpoint}:{port}/gremlin", "g")
 
 def lambda_handler(event, context):
-    userData = event["userInfo"]
-    firstName = userData["firstName"]
-    lastName = userData["lastName"]
-    dob = userData["DOB"]
-    username = userData["username"]
+    userData = event["request"]["userAttributes"]
+    # Inputs currently required by cognito
+    firstName = userData["given_name"]
+    lastName = userData["family_name"]
+    username = userData["preferred_username"]
     email = userData["email"]
-    gender = userData["gender"]
-    bio = userData["bio"]
 
+    # Inputs not required by cognito
+    dob = ""
+    gender = ""
+    bio = ""
+
+    # Add a user
     query = f"g.addV('user').property('firstName', '{firstName}').property('lastName', '{lastName}').property('DOB', '{dob}').property('username', '{username}').property('email', '{email}').property('gender', '{gender}').property('bio', '{bio}');"
-        
-    try:
-        db.submit(query)
-        result = f"User : {username} created successfully"
-        statusCode = 201
-    except:
-        result = f"Server failed at creating user : {username}"
-        statusCode = 500
+    db.submit(query)
+    # Get the user's vertex ID
+    query = f"g.V().has('username', '{username}').id();"
+    vertexID = db.submit(query).all().result()
+    # Add the vertex ID as a user property
+    query = f"g.V({vertexID}).property('id', '{vertexID[0]}');"
+    db.submit(query)
         
     # Process and return results
-    return {
-        "statusCode": statusCode,
-        "body": json.dumps(result)
-    }
+    return event
