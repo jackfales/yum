6eeedcfebbc1ccdec0c909fbeedef479d0c4e6f5
Derek Tran <tran.derek.d@gmail.com>
2024-01-25 17:08:59 -0800

    Fix Navbar to provide Profile Icon with user id

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const playfair = Playfair({
   weight: ['700']
 })
 
-export default function Navbar({username}: {username: String}) {
+export default function Navbar({username, userId}: {username: String, userId: String}) {
   const [prevScrollPos, setPrevScrollPos] = useState<number>(0);
   const [visible, setVisible] = useState<Boolean>(true);
 
@@ -34,7 +34,7 @@ export default function Navbar({username}: {username: String}) {
     <div className={`flex justify-between items-center fixed top-0 left-0 w-full h-12 px-5 shadow bg-rose-200 text-emerald-600 transition-transform ease duration-500 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
       <div>ICON</div>
       <h1 className={`${playfair.className} text-4xl`}>Yum</h1>
-      <ProfileIcon username={username}/>
+      <ProfileIcon username={username} userId={userId}/>
     </div>
   </>
   )
